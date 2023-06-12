06197eb2ed97f23e560b30e7e891a22e8e752373
Daniel Burgess <burgess.daniel4141@gmail.com>
2023-06-11 20:27:45 -0500

    Adding login modal to the landing page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,19 +1,37 @@
 <template>
   <div class="bg-black flex flex-col min-h-screen">
     <div class="w-full bg-sky-300 h-20 flex justify-between items-center">
-      <div>LOGO</div>
+      <div></div>
       <button class="mr-4 bg-blue-500 text-white px-4 py-2 rounded" @click="openLoginModal">Login</button>
       <!-- Other top navigation content -->
     </div>
     <div class="flex w-screen flex-grow">
       <div v-if="isLargeScreen" class="w-36 h-screen bg-cyan-100">side nav</div>
-      <div class="w-screen h-auto bg-green-50" @scroll="handleScroll">body</div>
+      <div class="w-screen h-auto bg-green-50" @scroll="handleScroll">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dolor metus, convallis id feugiat quis, finibus vel dolor. Nunc semper arcu leo, fermentum auctor turpis pretium quis. Duis lacinia, lacus sed porttitor laoreet, diam est interdum lacus, id consectetur est sapien eu mauris. Mauris a sem enim. Nullam vel fermentum justo, id feugiat est. Suspendisse ac sodales metus, ac consectetur diam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean nec nulla aliquam, fringilla ipsum at, ornare odio. Curabitur vel dignissim erat. Nulla et purus quis ligula pretium iaculis. Etiam iaculis, magna ac facilisis elementum, quam mauris scelerisque lacus, non laoreet purus nunc laoreet diam. Donec ac ante vel ligula porta sollicitudin vitae vitae massa. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam placerat iaculis tellus, et imperdiet lacus interdum eget.
+
+Proin vel nisi ut nulla posuere varius sagittis nec lorem. Curabitur eu elit ipsum. Sed placerat, ante venenatis aliquam accumsan, libero massa viverra magna, vel ullamcorper libero libero at nulla. Mauris accumsan consequat accumsan. Nullam sed velit posuere, vulputate leo eu, molestie lacus. Aliquam erat volutpat. Maecenas sit amet eleifend felis. Ut posuere tincidunt elit, vel consectetur enim vestibulum id. Maecenas malesuada tortor quis nunc mattis ullamcorper.
+
+Suspendisse ultricies mauris vitae augue pharetra, a egestas odio laoreet. Vivamus porttitor felis arcu. Nulla enim lorem, euismod lobortis eros id, vestibulum cursus nulla. In commodo accumsan aliquam. Praesent erat nulla, bibendum id quam non, scelerisque accumsan purus. Morbi eu ex non metus vehicula tempus vel at purus. Donec laoreet tincidunt vulputate. Nunc faucibus, nisl ac semper egestas, est magna condimentum lacus, vel pretium ligula mauris sed ligula. In facilisis ornare quam, in eleifend ligula porttitor ornare. In nec odio augue. Nam quis turpis ipsum. Vivamus tincidunt convallis feugiat.
+
+Ut massa mauris, euismod nec congue eu, venenatis eget augue. Vivamus ultricies libero sed pellentesque porta. Sed hendrerit a sapien id mattis. Duis sit amet ipsum molestie, egestas mauris vitae, condimentum lectus. In ultricies nunc et ipsum placerat eleifend. Vestibulum placerat, libero nec lobortis porttitor, lorem velit auctor metus, sed mollis nisi eros a metus. Ut id mauris ultricies, varius mauris eu, faucibus orci. Morbi ullamcorper rhoncus dolor eget pharetra.
+
+Morbi maximus rhoncus leo sed eleifend. Proin vitae est imperdiet, blandit tellus eu, interdum leo. Sed sodales elit sit amet erat aliquet vulputate. Integer id nibh mollis, faucibus tortor ac, scelerisque nisl. Donec sit amet lacinia purus. Aenean sed turpis tortor. Duis id ipsum in velit fringilla pulvinar ac at ligula. Suspendisse pharetra non arcu id convallis. Nunc risus augue, fermentum a lectus eu, iaculis rutrum dolor.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dolor metus, convallis id feugiat quis, finibus vel dolor. Nunc semper arcu leo, fermentum auctor turpis pretium quis. Duis lacinia, lacus sed porttitor laoreet, diam est interdum lacus, id consectetur est sapien eu mauris. Mauris a sem enim. Nullam vel fermentum justo, id feugiat est. Suspendisse ac sodales metus, ac consectetur diam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean nec nulla aliquam, fringilla ipsum at, ornare odio. Curabitur vel dignissim erat. Nulla et purus quis ligula pretium iaculis. Etiam iaculis, magna ac facilisis elementum, quam mauris scelerisque lacus, non laoreet purus nunc laoreet diam. Donec ac ante vel ligula porta sollicitudin vitae vitae massa. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam placerat iaculis tellus, et imperdiet lacus interdum eget.
+
+Proin vel nisi ut nulla posuere varius sagittis nec lorem. Curabitur eu elit ipsum. Sed placerat, ante venenatis aliquam accumsan, libero massa viverra magna, vel ullamcorper libero libero at nulla. Mauris accumsan consequat accumsan. Nullam sed velit posuere, vulputate leo eu, molestie lacus. Aliquam erat volutpat. Maecenas sit amet eleifend felis. Ut posuere tincidunt elit, vel consectetur enim vestibulum id. Maecenas malesuada tortor quis nunc mattis ullamcorper.
+
+Suspendisse ultricies mauris vitae augue pharetra, a egestas odio laoreet. Vivamus porttitor felis arcu. Nulla enim lorem, euismod lobortis eros id, vestibulum cursus nulla. In commodo accumsan aliquam. Praesent erat nulla, bibendum id quam non, scelerisque accumsan purus. Morbi eu ex non metus vehicula tempus vel at purus. Donec laoreet tincidunt vulputate. Nunc faucibus, nisl ac semper egestas, est magna condimentum lacus, vel pretium ligula mauris sed ligula. In facilisis ornare quam, in eleifend ligula porttitor ornare. In nec odio augue. Nam quis turpis ipsum. Vivamus tincidunt convallis feugiat.
+
+Ut massa mauris, euismod nec congue eu, venenatis eget augue. Vivamus ultricies libero sed pellentesque porta. Sed hendrerit a sapien id mattis. Duis sit amet ipsum molestie, egestas mauris vitae, condimentum lectus. In ultricies nunc et ipsum placerat eleifend. Vestibulum placerat, libero nec lobortis porttitor, lorem velit auctor metus, sed mollis nisi eros a metus. Ut id mauris ultricies, varius mauris eu, faucibus orci. Morbi ullamcorper rhoncus dolor eget pharetra.
+
+Morbi maximus rhoncus leo sed eleifend. Proin vitae est imperdiet, blandit tellus eu, interdum leo. Sed sodales elit sit amet erat aliquet vulputate. Integer id nibh mollis, faucibus tortor ac, scelerisque nisl. Donec sit amet lacinia purus. Aenean sed turpis tortor. Duis id ipsum in velit fringilla pulvinar ac at ligula. Suspendisse pharetra non arcu id convallis. Nunc risus augue, fermentum a lectus eu, iaculis rutrum dolor.
+      </div>
     </div>
     <div
       class="h-24 bg-green-500 fixed bottom-0 w-screen flex justify-between items-center"
       :class="{ 'hidden': hideBottomNav }"
     >
-      <div>LOGO</div>
+      <!-- <button class="ml-4 bg-red-500 text-white px-4 py-2 rounded">Left</button> -->
       <!-- Other bottom navigation content -->
       <button class="mr-4 bg-blue-500 text-white px-4 py-2 rounded" @click="openLoginModal">Login</button>
     </div>
